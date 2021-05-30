31d9ab3fe65c2748a8d9bc63de6fd1297ecc23bc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-30 06:05:44 +0000

    Deploy modlfo/VultModules to github.com/modlfo/VultModules.git:gh-pages

---
+++ js/grid.js
@@ -0,0 +1,566 @@
+var premium = [
+  {
+    slug: 'Basal',
+    name: 'Basal',
+    tags: ['VCO'],
+    description: 'Smooth Oscillator',
+    packages: ['Free']
+  },
+  {
+    slug: 'Bleak',
+    name: 'Bleak',
+    tags: ['VCO'],
+    description: 'Analog Oscillator',
+    packages: ['Free']
+  },
+  {
+    slug: 'Boomstick',
+    name: 'Boomstick',
+    tags: ['VCF', 'Polyphonic'],
+    description: 'Sallen-Key filter',
+    packages: ['Free', 'Premium']
+  },
+  {
+    slug: 'Caudal',
+    name: 'Caudal',
+    tags: ['Random', 'LFO', 'Noise'],
+    description: 'Mechanical Chaos Source',
+    packages: ['Free']
+  },
+  {
+    slug: 'Debriatus',
+    name: 'Debriatus',
+    tags: ['Distortion', 'Waveshaper', 'Effect'],
+    description: 'Wave Destruction',
+    packages: ['Free']
+  },
+  {
+    slug: 'Decline',
+    name: 'Decline',
+    tags: ['Equalizer'],
+    description: 'Equalizer Line',
+    packages: ['Free']
+  },
+  {
+    slug: 'Disjoint',
+    name: 'Disjoint',
+    tags: ['Equalizer', 'VCF', 'Polyphonic'],
+    description: 'DJ Style Filter',
+    packages: ['Free', 'Premium']
+  },
+  {
+    slug: 'Dopamine',
+    name: 'Dopamine',
+    tags: ['Sequencer', 'Random'],
+    description: 'Neural Tape',
+    packages: ['Mysteries']
+  },
+  {
+    slug: 'Lapsus',
+    name: 'Lapsus',
+    tags: ['Sequencer'],
+    description: 'Non-Euclidean Rhythm Generator',
+    packages: ['Mysteries']
+  },
+  {
+    slug: 'Flame',
+    name: 'Flame',
+    tags: ['Distortion', 'Waveshaper', 'Effect'],
+    description: 'Analog Distortion',
+    packages: ['Premium']
+  },
+  {
+   slug: 'Ferox',
+   name: 'Ferox',
+   tags: ['VCF'],
+   description: 'CMOS Filter',
+   packages: ['Premium']
+ },{
+    slug: 'Feigen',
+    name: 'Feigen',
+    tags: ['Random'],
+    description: 'Bifurcations Generator',
+    packages: ['Mysteries']
+  },
+  {
+    slug: 'Flux',
+    name: 'Flux',
+    tags: [
+      'Attenuator', 'Envelope follower', 'Quad', 'Sample and hold',
+      'Slew limiter', 'Utility'
+    ],
+    description: 'Quad Voltage Processor',
+    packages: ['Free']
+  },
+  {
+    slug: 'HyperPower',
+    name: 'HyperPower',
+    tags: ['Utility'],
+    description: 'Infinite Power Supply',
+    packages: ['Premium']
+  },
+  {
+    slug: 'Julste',
+    name: 'Julste',
+    tags: ['Low pass gate'],
+    description: 'Simple Low Pass Gate',
+    packages: ['Free']
+  },
+  {
+    slug: 'Lateralus',
+    name: 'Lateralus',
+    tags: ['VCF'],
+    description: 'Diode Ladder Filter',
+    packages: ['Free', 'Premium']
+  },
+  {
+    slug: 'Leakage',
+    name: 'Leakage',
+    tags: [
+      'Attenuator', 'Envelope follower', 'Sample and hold', 'Slew limiter',
+      'Utility', 'Random', 'Noise'
+    ],
+    description: 'Voltage Processor',
+    packages: ['Free']
+  },
+  {
+    slug: 'Nopskate',
+    name: 'Nopskate',
+    tags: ['Waveshaper', 'Effect'],
+    description: 'Flip-Flop Octaver',
+    packages: ['Free']
+  },
+  {
+    slug: 'Noxious',
+    name: 'Noxious',
+    tags: ['VCO', 'LFO'],
+    description: 'Pure Distortion Oscillator',
+    packages: ['Premium']
+  },
+  {
+    slug: 'Noxious-Poly',
+    name: 'Noxious-Poly',
+    tags: ['VCO', 'LFO', 'Polyphonic'],
+    description: 'Polyphonic Noxious',
+    packages: ['Premium'],
+    link: 'Noxious'
+  },
+  {
+    slug: 'Nurage',
+    name: 'Nurage',
+    tags: ['Low pass gate', 'VCF', 'VCA'],
+    description: 'Dual Low Pass Gate/Filter',
+    packages: ['Premium']
+  },
+  {
+    slug: 'Rescomb',
+    name: 'Rescomb',
+    tags: ['VCF', 'Effect'],
+    description: 'Resonant Comb Filter',
+    packages: ['Free']
+  },
+  {
+    slug: 'Rescomb2',
+    name: 'Rescomb 2',
+    tags: ['VCF', 'Effect', 'Physical modeling'],
+    description: 'Resonant Comb Filter',
+    packages: ['Premium']
+  },
+  {
+    slug: 'Slap',
+    name: 'Slap',
+    tags: ['Envelope generator', 'VCA'],
+    description: 'Hitting Envelope',
+    packages: ['Free']
+  },
+  {
+    slug: 'Spank',
+    name: 'Spank',
+    tags: ['Envelope generator', 'VCA'],
+    description: 'Drum Envelope',
+    packages: ['Free']
+  },
+  {
+    slug: 'Splie',
+    name: 'Splie',
+    tags: ['Multiple', 'Utility'],
+    description: 'Active Buffer',
+    packages: ['Free']
+  },
+  {
+    slug: 'Stabile',
+    name: 'Stabile',
+    tags: ['VCF'],
+    description: 'State Variable Filter',
+    packages: ['Free']
+  },
+  {
+    slug: 'Tangents',
+    name: 'Tangents',
+    tags: ['VCF'],
+    description: 'Steiner-Parker Filter',
+    packages: ['Free', 'Premium']
+  },
+  {
+    slug: 'Tohe',
+    name: 'Tohe',
+    tags: ['VCF', 'Equalizer'],
+    description: 'Tone Control',
+    packages: ['Free']
+  },
+  {
+    slug: 'Trummor',
+    name: 'Trummor',
+    tags: ['Drum', 'Envelope generator', 'Noise'],
+    description: 'Drum Synthesizer',
+    packages: ['Free']
+  },
+  {
+    slug: 'Trummor2',
+    name: 'Trummor 2',
+    tags: ['Drum', 'Envelope generator', 'Noise'],
+    description: 'Percussion Synthesizer',
+    packages: ['Free']
+  },
+  {
+    slug: 'Unstabile',
+    name: 'Unstabile',
+    tags: ['VCF'],
+    description: 'Bent State Variable Filter',
+    packages: ['Free']
+  },
+  {
+    slug: 'Vessek',
+    name: 'Vessek',
+    tags: ['VCO'],
+    description: 'Complex Analog Oscillator',
+    packages: ['Free']
+  },
+  {
+    slug: 'Vortex',
+    name: 'Vortex',
+    tags: ['VCF'],
+    description: 'Russian Filter',
+    packages: ['Premium']
+  },
+  {
+    slug: 'Quincunx',
+    name: 'Quincunx',
+    tags: ['Sequencer', 'Random'],
+    description: 'Probability Machine',
+    packages: ['Mysteries']
+  },
+  {
+    slug: 'BlackPanelSmall',
+    name: 'Blank: 3 HP',
+    tags: ['Blank'],
+    description: 'Blank: 3 HP',
+    packages: ['Free'],
+    link: 'panels'
+  },
+  {
+    slug: 'BlackPanel',
+    name: 'Blank: 6 HP',
+    tags: ['Blank'],
+    description: 'Blank: 6 HP',
+    packages: ['Free'],
+    link: 'panels'
+  },
+  {
+    slug: 'BlackPanelBig',
+    name: 'Blank: 12 HP',
+    tags: ['Blank'],
+    description: 'Blank: 12 HP',
+    packages: ['Free'],
+    link: 'panels'
+  },
+  {
+    slug: 'UtilBypass',
+    name: 'Bypass',
+    tags: ['Utility'],
+    description: 'Stereo Channel Effect Bypass',
+    packages: ['Free'],
+    link: 'Bypass'
+  },
+  {
+    slug: 'UtilKnobs',
+    name: 'Knobs',
+    tags: ['Utility', 'Attenuator', 'Mixer', 'Dual'],
+    description: 'Modulation Mixer and Macro Control',
+    packages: ['Free'],
+    link: 'Knobs'
+  },
+  {
+    slug: 'UtilSend',
+    name: 'Send',
+    tags: ['Utility'],
+    description: 'Stereo Channel Send/Return',
+    packages: ['Free'],
+    link: 'Send'
+  },
+  {
+   slug: 'Wolv',
+   name: 'Wolv',
+   tags: ['Waveshaper'],
+   description: 'Waveshaper',
+   packages: ['Free'],
+   link: 'Wolv'
+ }
+];
+
+var compacts = [
+  {
+    slug: 'Freak',
+    name: 'Freak',
+    tags: ['VCF'],
+    description: 'Manifold Filter',
+    packages: ['Compacts']
+  },
+  {
+   slug: 'Freak-HW',
+   name: 'Freak-HW',
+   tags: ['VCF'],
+   description: 'Manifold Filter',
+   packages: ['Compacts']
+ },
+  {
+    slug: 'Incubus',
+    name: 'Incubus',
+    tags: ['Synth Voice'],
+    description: 'Nightmare Synthesizer',
+    packages: ['Compacts']
+  },
+  {
+    slug: 'Knock',
+    name: 'Knock',
+    tags: ['Drum'],
+    description: 'Bass Drum',
+    packages: ['Compacts']
+  },
+  {
+    slug: 'Vorg',
+    name: 'Vorg',
+    tags: ['VCF'],
+    description: 'Low/High Pass Filter',
+    packages: ['Compacts']
+  },
+  {
+   slug: 'Vraids',
+   name: 'Vraids',
+   tags: ['VCO'],
+   description: 'Macro oscillator',
+   packages: ['Compacts']
+ }
+];
+
+var modules = premium.concat(compacts).sort(function(a, b) {
+  if (a.name < b.name)
+    return -1;
+  else
+    return 1;
+});
+
+function uniq(a) {
+  let seen = new Set();
+  return a.filter(item => {
+    let k = item;
+    return seen.has(k) ? false : seen.add(k);
+  });
+}
+
+var tags = uniq(Array.prototype.concat.apply([], modules.map(x => x.tags)))
+               .sort(function(a, b) {
+                 if (a < b)
+                   return -1;
+                 else
+                   return 1;
+               });
+
+var packages = ['All', 'Premium', 'Free', 'Compacts', 'Mysteries'];
+
+var prefix = '';
+
+function addModuleElement(m) {
+  var div = document.createElement('a');
+  div.className = 'ModuleGridElement'
+  div.id = m.slug;
+  if (m.link != undefined) {
+    div.href = prefix + m.link.toLowerCase();
+
+  } else {
+    div.href = prefix + m.slug.toLowerCase();
+  }
+
+  var title = document.createElement('div');
+  title.innerHTML = m.name;
+  title.className = 'ModuleName';
+  div.appendChild(title);
+
+  var description = document.createElement('div');
+  description.innerHTML = m.description;
+  description.className = 'ModuleDescription';
+  div.appendChild(description);
+
+  var img = document.createElement('img');
+  img.src = prefix + 'images/' + m.slug + '.png';
+  div.appendChild(img);
+
+  document.getElementById('module-grid').appendChild(div);
+}
+
+var active_package_filter = 'All';
+var active_tags_filter = [];
+
+
+function activeTag(id) {
+  var index = -1;
+  for (var i = 0; i < active_tags_filter.length; i++) {
+    if (active_tags_filter[i] == id) {
+      index = i;
+    }
+  }
+  return index;
+}
+
+function keepModule(m) {
+  var cond1 = active_package_filter == 'All';
+  var cond2 = active_tags_filter.length == 0 ? true : false;
+  // By Tags
+  for (var i = 0; i < m.tags.length; i++) {
+    var tag = m.tags[i];
+    if (activeTag(tag) >= 0) {
+      cond2 = true;
+    }
+  }
+  // By Package
+  for (var i = 0; i < m.packages.length; i++) {
+    if (m.packages[i] == active_package_filter) {
+      cond1 = true;
+    }
+  }
+  return cond1 && cond2;
+}
+
+function filterModules() {
+  for (var i = 0; i < modules.length; i++) {
+    var m = modules[i];
+    var keep = keepModule(m);
+    var elem = document.getElementById(m.slug);
+    elem.hidden = !keep;
+  }
+}
+
+function packageFilter(name) {
+  // Deselect all buttons
+  for (var i = 0; i < packages.length; i++) {
+    var elem = document.getElementById(packages[i] + '-filter');
+    elem.className = 'FilterButton';
+  }
+  // Select the active
+  var elem = document.getElementById(name + '-filter');
+  elem.className = 'FilterButtonActive';
+
+  active_package_filter = name;
+  filterModules();
+}
+
+function tagFilter(id) {
+  var index = activeTag(id);
+  if (index >= 0) {
+    active_tags_filter.splice(index, 1);
+  } else {
+    active_tags_filter.push(id);
+  }
+  for (var i = 0; i < tags.length; i++) {
+    var elem = document.getElementById(tags[i] + '-filter');
+    if (activeTag(tags[i]) >= 0) {
+      elem.className = 'FilterButtonActive';
+    } else {
+      elem.className = 'FilterButton';
+    }
+  }
+  console.log(active_tags_filter);
+  filterModules();
+}
+
+function addButton(name, to, callback) {
+  var div = document.createElement('div');
+  div.style.marginTop = '5px';
+  div.style.marginBottom = '5px';
+  div.style.marginLeft = '2px';
+  div.style.marginRight = '2px';
+
+  div.style.float = 'left'
+  var b = document.createElement('a');
+  b.id = name + '-filter';
+  b.className = 'FilterButton';
+  b.role = 'button';
+  b.innerHTML = name;
+  b.style.color = 'rgb(255, 255, 255)';
+  b.style.textDecoration = 'none';
+  b.onclick = function() {
+    callback(name);
+  };
+  b.href = 'javascript:void(null);';
+  div.appendChild(b);
+  document.getElementById(to).appendChild(div);
+}
+
+function addButtons() {
+  var div = document.createElement('div');
+  div.style.float = 'left'
+  div.innerHTML = 'Packages:';
+  document.getElementById('module-buttons').appendChild(div);
+
+  for (var i = 0; i < packages.length; i++) {
+    addButton(packages[i], 'module-buttons', packageFilter);
+  }
+
+  var tags_container = document.createElement('div');
+  tags_container.innerHTML = 'Tags:';
+  tags_container.style.float = 'left'
+  document.getElementById('module-tags').appendChild(tags_container);
+
+  for (var i = 0; i < tags.length; i++) {
+    addButton(tags[i], 'module-tags', tagFilter);
+  }
+}
+
+function populateGrid() {
+  addButtons();
+
+  var grid = document.createElement('div');
+  grid.id = 'module-grid';
+
+  document.getElementById('modules').appendChild(grid);
+
+  for (var i = 0; i < modules.length; i++) {
+    addModuleElement(modules[i]);
+  }
+
+  packageFilter('All');
+}
+
+function populatePremium() {
+  prefix = '../';
+  populateGrid();
+  packageFilter('Premium');
+}
+
+function populateFree() {
+  prefix = '../';
+  populateGrid();
+  packageFilter('Free');
+}
+
+function populateCompacts() {
+  prefix = '../';
+  populateGrid();
+  packageFilter('Compacts');
+}
+
+function populateMysteries() {
+  prefix = '../';
+  populateGrid();
+  packageFilter('Mysteries');
+}
